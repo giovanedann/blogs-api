8c6c55c8c34f0bf48ace22bc8ef28a1771de85dc
Giovane <giovanedaniell@gmail.com>
2022-08-27 13:53:55 -0300

    feat: create blogPost model

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,36 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    "BlogPost",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: true,
+      tableName: "BlogPosts",
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: "userId",
+      as: "user",
+    });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
