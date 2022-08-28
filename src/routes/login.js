39874479abf24b4f8a3c73d68e110fc624b278ac
Giovane <giovanedaniell@gmail.com>
2022-08-28 14:56:31 -0300

    feat(routes): add authentication middleware on login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
+const authentication = require('../middlewares/authentication');
 
 const loginRoutes = Router();
 
-loginRoutes.post('/', () => {});
+loginRoutes.post('/login', authentication);
 
 module.exports = loginRoutes;
