02097d3006cea6196e2a278a004a9569de039ec3
Giovane <giovanedaniell@gmail.com>
2022-08-28 14:58:58 -0300

    feat(helpers): create validateUserCredentials helper

---
+++ src/helpers/validateUserCredentials.js
@@ -0,0 +1,24 @@
+const isEmailValid = require('../utils/isEmailValid');
+const validateLength = require('../utils/validateLength');
+
+module.exports = ({ displayName, email, password }) => {
+  if (!isEmailValid(email)) {
+    return { statusCode: 400, message: '"email" must be a valid email' };
+  }
+
+  if (!validateLength(password, 6)) {
+    return {
+      statusCode: 400,
+      message: '"password" length must be at least 6 characters long',
+    };
+  }
+
+  if (!validateLength(displayName, 8)) {
+    return {
+      statusCode: 400,
+      message: '"displayName" length must be at least 8 characters long',
+    };
+  }
+
+  return null;
+};
