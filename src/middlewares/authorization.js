4960794718abfad786ceddd9db27a734f5ebba7a
Giovane <giovanedaniell@gmail.com>
2022-08-28 16:11:28 -0300

    refactor: authorization middleware

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -4,19 +4,18 @@ const { User } = require('../database/models');
 
 module.exports = async (request, response, next) => {
   const { authorization } = request.headers;
-
   if (!authorization) {
     return response.status(401).json({ message: 'Token not found' });
   }
 
   try {
     const { data } = jwt.verify(authorization, process.env.JWT_SECRET);
-
     const userExists = await User.findOne({ where: { email: data } });
-
-    if (userExists) {
-      next();
+    if (!userExists) {
+      return response.status(401).json({ message: 'Expired or invalid token' });
     }
+
+    next();
   } catch (e) {
     return response.status(401).json({ message: 'Expired or invalid token' });
   }
