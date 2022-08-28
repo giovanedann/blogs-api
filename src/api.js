775d35f1ced3174669d59402509fa6457e70ecda
Giovane <giovanedaniell@gmail.com>
2022-08-28 15:00:37 -0300

    feat: add userRoutes and loginRoutes to the application

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
-// ...
+const loginRoutes = require('./routes/login');
+const userRoutes = require('./routes/user');
 
 const app = express();
-
 app.use(express.json());
 
-// ...
+app.use(loginRoutes);
+app.use(userRoutes);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
