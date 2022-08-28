d75ab70219594c16155efb1c0e3ff476692b947f
Giovane <giovanedaniell@gmail.com>
2022-08-28 16:12:04 -0300

    feat(routes): added get /user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const UserController = require('../controllers/UserController');
-// const authorization = require('../middlewares/authorization');
+const authorization = require('../middlewares/authorization');
 
 const userRoutes = Router();
 
+userRoutes.get('/user', authorization, UserController.index);
 userRoutes.post('/user', UserController.store);
 
 module.exports = userRoutes;
