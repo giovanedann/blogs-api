a555147914db1e529ad26a24643121496f33ffc9
Giovane <giovanedaniell@gmail.com>
2022-08-28 14:58:32 -0300

    refactor(middleware): using generateJwtToken function on authentication middleware

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,15 +1,11 @@
-require('dotenv');
-
-const jwt = require('jsonwebtoken');
+const generateJwtToken = require('../utils/generateJwtToken');
 const { User } = require('../database/models');
 
 module.exports = async (request, response, next) => {
   const { email, password } = request.body;
 
   if (!email || !password) {
-    return response
-      .status(400)
-      .json({ message: 'Some required fields are missing' });
+    return response.status(400).json({ message: 'Some required fields are missing' });
   }
 
   const userExists = await User.findOne({ where: { email } });
@@ -18,9 +14,7 @@ module.exports = async (request, response, next) => {
     return response.status(400).json({ message: 'Invalid fields' });
   }
 
-  const token = jwt.sign({ data: email }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
-  });
+  const token = generateJwtToken(email);
 
   response.status(200).json({ token });
   next();
