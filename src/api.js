d029ec2f1a7535da647190ae0f5104655a016f78
Giovane <giovanedaniell@gmail.com>
2022-08-29 17:55:19 -0300

    feat(app): implemented categoryRoutes on the application

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const loginRoutes = require('./routes/login');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
 
 const app = express();
 app.use(express.json());
 
 app.use(loginRoutes);
 app.use(userRoutes);
+app.use(categoryRoutes);
 
 module.exports = app;
