7b3ebf6a5493dd22e46503e556a63c4df3b6fb35
Giovane <giovanedaniell@gmail.com>
2022-09-01 19:03:44 -0300

    feat(routes): :sparkles: create get routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,5 +4,7 @@ const BlogPostController = require('../controllers/BlogPostController');
 
 const blogPostRoutes = Router();
 
+blogPostRoutes.get('/post/:id', authorization, BlogPostController.show);
+blogPostRoutes.get('/post', authorization, BlogPostController.index);
 blogPostRoutes.post('/post', authorization, BlogPostController.store);
 module.exports = blogPostRoutes;
