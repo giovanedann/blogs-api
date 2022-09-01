159ece8409aebb87494828179ab243bc641b0fc1
Giovane <giovanedaniell@gmail.com>
2022-09-01 19:00:49 -0300

    feat(services): findAll and findOne method on BlogPost service

--- src/services/BlogPostService.js
+++ src/services/BlogPostService.js
@@ -1,6 +1,47 @@
-const { BlogPost } = require('../database/models');
+const { BlogPost, User, Category } = require('../database/models');
 
 class BlogPostService {
+  static async findAll() {
+    const blogPosts = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: {
+            exclude: ['password'],
+          },
+        },
+        {
+          model: Category,
+          as: 'categories',
+          through: { attributes: [] },
+        },
+      ],
+    });
+    return blogPosts;
+  }
+
+  static async findOne(id) {
+    const blogPosts = await BlogPost.findOne({
+      where: { id },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: {
+            exclude: ['password'],
+          },
+        },
+        {
+          model: Category,
+          as: 'categories',
+          through: { attributes: [] },
+        },
+      ],
+    });
+    return blogPosts;
+  }
+
   static async createPost({ title, content, userId }) {
     const postData = await BlogPost.create({
       title,
