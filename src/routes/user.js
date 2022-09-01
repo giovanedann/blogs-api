5693beb433487f946dc309c382701d88b92beaec
Giovane <giovanedaniell@gmail.com>
2022-09-01 20:17:38 -0300

    feat(routes): :sparkles: create User deactivate own accound route

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,7 @@ const authorization = require('../middlewares/authorization');
 const userRoutes = Router();
 
 userRoutes.get('/user/:id', authorization, UserController.show);
+userRoutes.delete('/user/me', authorization, UserController.deactivate);
 userRoutes.get('/user', authorization, UserController.index);
 userRoutes.post('/user', UserController.store);
 
